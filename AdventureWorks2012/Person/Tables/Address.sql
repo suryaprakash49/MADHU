149bca99929a2774034d0a8292f88b802b9a57b4
suryaprakash49 <138649866+suryaprakash49@users.noreply.github.com>
2024-05-28 21:25:20 +0530

    Update Address.sql

--- AdventureWorks2012/Person/Tables/Address.sql
+++ AdventureWorks2012/Person/Tables/Address.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [Person].[Address] (
     [AddressID]       INT               IDENTITY (1, 1) NOT FOR REPLICATION NOT NULL,
-    [AddressLine1]    NVARCHAR (60)     NOT NULL,
+    [AddressLine1]    NVARCHAR (80)     NOT NULL,
     [AddressLine2]    NVARCHAR (60)     NULL,
     [City]            NVARCHAR (30)     NOT NULL,
     [StateProvinceID] INT               NOT NULL,
